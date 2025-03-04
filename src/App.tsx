2d5ede6fdf88dae2a7e4db17ff9c2081d217b39a
CILet99 <letourneauc@mssm.org>
2025-03-03 20:39:56 -0500

    ch2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
-        <div className="App">
+        <div
+            className="App"
+            style={{ border: "4px solid red", padding: "4px" }}
+        >
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
-            <p>Hello World</p>
+
+            <Container>
+                <Col
+                    style={{
+                        height: "80px",
+                        width: "80px",
+                        backgroundColor: "red",
+                    }}
+                >
+                    <ul>
+                        <li>one</li>
+                        <li>two</li>
+                        <li>four</li>
+                    </ul>
+                </Col>
+            </Container>
+
+            <img alt="text" />
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <h1>Hello World</h1>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
